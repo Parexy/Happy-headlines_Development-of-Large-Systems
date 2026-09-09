2ea1c60888ae75b4f7a2a941fdd0c9111abd71a6
Lukas <lukas@askholm.net>
2026-09-09 12:31:24 +0200

    edit docker compose and add sql init for new services

--- HappyHeadlines/database/ArticleInit.sql
+++ HappyHeadlines/database/ArticleInit.sql
@@ -14,3 +14,6 @@ CREATE TABLE IF NOT EXISTS "Articles"
 
     "UpdatedAt" timestamp with time zone NOT NULL
 );
+
+CREATE INDEX IF NOT EXISTS "IX_Articles_PublishedAt"
+    ON "Articles" ("PublishedAt" DESC);
